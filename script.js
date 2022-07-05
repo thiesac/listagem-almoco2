319389fe8782240bd6602739757bae9a0e6e5b8a
thiesac <thiesa.c@gmail.com>
2022-07-04 21:55:46 -0400

    cardapio atualizado

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
-let cardapio = []
 let cardapioBkp
+let cardapio = [
+    {
+        descricao: "brownie",
+        valor: 12,
+        imagem: "https://akdelicatessen.com.br/wp-content/uploads/2022/04/Brownie-de-Nescau-TC.jpg"
+    },
+    {
+        descricao: "panqueca americana",
+        valor: 22,
+        imagem: "https://img-global.cpcdn.com/recipes/c667062f7f96d825/400x400cq70/photo.jpg"
+    },
+    {
+        descricao: "waffle",
+        valor: 20,
+        imagem: "https://receitatodahora.com.br/wp-content/uploads/2021/11/waffle-americano-scaled.jpg"
+    },
+    {
+        descricao: "cappuccino",
+        valor: 7,
+        imagem: "https://guiadaculinaria.com/wp-content/uploads/2021/04/receita-de-cappuccino-caseiro.jpg"
+    },
+    {
+        descricao: "café",
+        valor: 3,
+        imagem: "https://s2.glbimg.com/3uxgM2qRPi16CrOUbJrxpYEK2iU=/e.glbimg.com/og/ed/f/original/2020/04/22/table-liquid-cafe-coffee-white-morning-659632-pxhere.com.jpg"
+    }
+
+]
 
 function adicionar() {
     let insereProduto = {
